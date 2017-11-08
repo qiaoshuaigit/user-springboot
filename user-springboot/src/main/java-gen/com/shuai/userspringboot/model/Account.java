9c7502c0fc31629d5ca9082f0178c8abee253cc0
乔帅 <qsxcc@126.com>
2017-11-08 09:08:34 +0800

    添加mybatis自动生成代码插件

---
+++ user-springboot/src/main/java-gen/com/shuai/userspringboot/model/Account.java
@@ -0,0 +1,33 @@
+package com.shuai.userspringboot.model;
+
+public class Account {
+    private Integer id;
+
+    private String money;
+
+    private String name;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getMoney() {
+        return money;
+    }
+
+    public void setMoney(String money) {
+        this.money = money == null ? null : money.trim();
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name == null ? null : name.trim();
+    }
+}
