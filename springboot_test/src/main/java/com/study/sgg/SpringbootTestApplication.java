7885864b76d9f85d5ebc3bcf1ee951b27d8582b3
乔帅 <qsxcc@126.com>
2018-04-24 09:17:33 +0800

    新增模块用于【系统学习springboot】
    	new file:   springboot_test/.gitignore
    	new file:   springboot_test/.mvn/wrapper/maven-wrapper.jar
    	new file:   springboot_test/.mvn/wrapper/maven-wrapper.properties
    	new file:   springboot_test/mvnw
    	new file:   springboot_test/mvnw.cmd
    	new file:   springboot_test/pom.xml
    	new file:   springboot_test/src/main/java/com/study/sgg/SpringbootTestApplication.java
    	new file:   springboot_test/src/main/resources/application.properties
    	new file:   springboot_test/src/test/java/com/study/sgg/SpringbootTestApplicationTests.java

---
+++ springboot_test/src/main/java/com/study/sgg/SpringbootTestApplication.java
@@ -0,0 +1,12 @@
+package com.study.sgg;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringbootTestApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringbootTestApplication.class, args);
+	}
+}
